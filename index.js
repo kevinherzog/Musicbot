1e470d348ce39b9a8d38ae73b7e55e806154f7a4
Kevin <quotenslav@gmail.com>
2020-07-28 16:17:24 +0200

    Finalize index.js

--- index.js
+++ index.js
@@ -43,14 +43,13 @@ client.on('message', message => {
 	if (!command) return;
 
 
-    /* Look if command has all needed components */
+    /* Look if command has all needed components (May be removed in the future, if juged to be not needed) */
 	if (command.args && !args.length) {
 		let reply = `You didn't provide any arguments, ${message.author}!`;
 
 		if (command.usage) {
 			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
 		}
-
 		return message.channel.send(reply);
     }
 
